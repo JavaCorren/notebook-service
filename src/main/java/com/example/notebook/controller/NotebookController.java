bc773365bc2e5b0f78c7fb6f38c12267b55f4cf5
Corren ZHANG <corren2012@sina.com>
2023-03-20 16:41:26 +1300

    force commit

--- src/main/java/com/example/notebook/controller/NotebookController.java
+++ src/main/java/com/example/notebook/controller/NotebookController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
-import javax.validation.valueextraction.ExtractedValue;
 
 @Slf4j
 @Controller
